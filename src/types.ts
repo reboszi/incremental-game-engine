d41a7519d361735e836af694faf8f6c2e82b4f22
reboszi <reboszi@gmail.com>
2026-09-18 22:37:09 +0200

    Add core game project types

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export type GameSettings = {
+  defaultPanelBackgroundColor: string
+  defaultPanelTextColor: string
+  defaultPanelBorderColor: string
+}
+
+export type PanelSlot =
+  | 'top'
+  | 'top-inner'
+  | 'left-outer'
+  | 'left-inner'
+  | 'center'
+  | 'right-inner'
+  | 'right-outer'
+  | 'bottom-inner'
+  | 'bottom'
+
+export type Panel = {
+  id: string
+  title: string
+  slot: PanelSlot
+  backgroundColor: string
+  textColor: string
+  borderColor: string
+}
+
+export type GameProject = {
+  version: number
+  name: string
+  gameSettings: GameSettings
+  panels: Panel[]
+}
