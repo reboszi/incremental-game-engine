d061f828d6762c0ac64fcef63ad44b611f14af0a
reboszi <reboszi@gmail.com>
2026-09-20 20:37:00 +0200

    Add generic resource model

--- src/types.ts
+++ src/types.ts
@@ -24,12 +24,25 @@ export type Panel = {
   borderColor: string
 }
 
+export type ResourceDisplayMode = 'value' | 'value-max' | 'bar'
+
+export type Resource = {
+  id: string
+  name: string
+  initialValue: number
+  maxValue: number | null
+  unit: string
+  displayMode: ResourceDisplayMode
+  initiallyVisible: boolean
+}
+
 export type GameProject = {
   id: string
   version: number
   name: string
   gameSettings: GameSettings
   panels: Panel[]
+  resources: Resource[]
 }
 
 export type ProjectSummary = {
